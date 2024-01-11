4d80afa0d8538628cce8e1f458d69652bb9bc4d7
JOAO LUCAS MELO <lucasmelo59@unifor.br>
2024-01-11 17:26:10 -0300

    construindo api rest, controller

--- src/main/java/org/br/mineradora/controller/ProposalController.java
+++ src/main/java/org/br/mineradora/controller/ProposalController.java
@@ -1,14 +1,59 @@
 package org.br.mineradora.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
+import jakarta.persistence.GeneratedValue;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import org.br.mineradora.dto.ProposalDetailsDTO;
 import org.br.mineradora.service.ProposalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Path("api/proposta")
 public class ProposalController {
 
+    private final Logger LOG = LoggerFactory.getLogger(ProposalController.class);
 
     @Inject
     ProposalService proposalService;
 
+    @GET
+    @Path("/{id}")
+    public ProposalDetailsDTO findDetailsProposal(@PathParam("id") long id) {
+        return proposalService.findFullProposal(id);
+    }
 
+    @POST
+//    @RolesAllowed("")
+    public Response createProposal(ProposalDetailsDTO proposalDetailsDTO) {
+
+        LOG.info("--- Recebendo proposta de compra ---");
+
+        try {
+
+            proposalService.createNewProposal(proposalDetailsDTO);
+            return Response.ok().build();
+        } catch (Exception e) {
+
+            return Response.serverError().build();
+
+        }
+
+    }
+
+
+    @DELETE
+    @Path("/{id}")
+//    @RolesAllowed("manager")
+    public Response removeProposal(@PathParam("id") long id) {
+
+        try {
+            proposalService.removeProposal(id);
+            return Response.ok().build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
 
 }
