9384bb27832d1923a058099d26c58a36d439ee72
JOAO LUCAS MELO <lucasmelo59@unifor.br>
2024-01-11 16:53:47 -0300

    finalizando service

--- src/main/java/org/br/mineradora/controller/ProposalController.java
+++ src/main/java/org/br/mineradora/controller/ProposalController.java
@@ -9,4 +9,6 @@ public class ProposalController {
     @Inject
     ProposalService proposalService;
 
+
+
 }
---
+++ src/main/java/org/br/mineradora/service/ProposalServiceImpl.java
@@ -0,0 +1,80 @@
+package org.br.mineradora.service;
+
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.br.mineradora.dto.ProposalDTO;
+import org.br.mineradora.dto.ProposalDetailsDTO;
+import org.br.mineradora.entity.ProposalEntity;
+import org.br.mineradora.message.KafkaEvent;
+import org.br.mineradora.repository.ProposalRepository;
+
+import java.time.LocalDateTime;
+
+public class ProposalServiceImpl implements ProposalService{
+
+    @Inject
+    ProposalRepository proposalRepository;
+
+    @Inject
+    KafkaEvent kafkaEvent;
+
+    @Override
+    public ProposalDetailsDTO findFullProposal(long id) {
+
+        ProposalEntity proposal = proposalRepository.findById(id);
+
+        return ProposalDetailsDTO.builder()
+                .proposalId(proposal.getId())
+                .country(proposal.getCountry())
+                .customer(proposal.getCustomer())
+                .proposalValidityDays(proposal.getProposalValidityDays())
+                .tonnes(proposal.getTonnes())
+                .priceTonne(proposal.getPriceTonne())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public void createNewProposal(ProposalDetailsDTO proposalDetailsDTO) {
+
+        ProposalDTO proposalDTO = buildAndSaveNewProposal(proposalDetailsDTO);
+        kafkaEvent.sendNewKafkaEvent(proposalDTO);
+
+    }
+
+    @Override
+    @Transactional
+    public void removeProposal(long id) {
+        proposalRepository.deleteById(id);
+    }
+
+    @Transactional
+    private ProposalDTO buildAndSaveNewProposal(ProposalDetailsDTO proposalDetailsDTO) {
+        try {
+
+            ProposalEntity proposal = new ProposalEntity();
+
+            proposal.setCreated(LocalDateTime.now());
+            proposal.setProposalValidityDays(proposalDetailsDTO.getProposalValidityDays());
+            proposal.setCountry(proposalDetailsDTO.getCountry());
+            proposal.setCustomer(proposal.getCustomer());
+            proposal.setPriceTonne(proposalDetailsDTO.getPriceTonne());
+            proposal.setTonnes(proposal.getTonnes());
+
+            proposalRepository.persist(proposal);
+
+            return ProposalDTO.builder()
+                    .proposalId(proposalRepository.findByCustomer(proposal.getCustomer()).get().getId())
+                    .priceTonne(proposal.getPriceTonne())
+                    .customer(proposal.getCustomer())
+                    .build();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            throw new RuntimeException();
+
+        }
+    }
+
+
+}
