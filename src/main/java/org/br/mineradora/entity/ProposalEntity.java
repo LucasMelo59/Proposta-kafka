7c1b48ecf877d41816c1fdc4a686f5ad58a5f4cf
JOAO LUCAS MELO <lucasmelo59@unifor.br>
2024-01-11 11:13:20 -0300

    initial commit

---
+++ src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -0,0 +1,35 @@
+package org.br.mineradora.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "proposal")
+@Data
+@NoArgsConstructor
+public class ProposalEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String customer;
+
+    @Column(name = "price_tonne")
+    private BigDecimal priceTonne;
+
+    private Integer tonnes;
+
+    private String country;
+
+    @Column(name = "proposal_validity_days" )
+    private Integer proposalValidityDays;
+
+    private LocalDateTime created;
+
+}
