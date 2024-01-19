3260d7fc8a108bdadef4254929bf1b8f6ed4d461
JOAO LUCAS MELO <lucasmelo59@unifor.br>
2024-01-19 10:40:38 -0300

    adicionando authorization

--- src/main/java/org/br/mineradora/controller/ProposalController.java
+++ src/main/java/org/br/mineradora/controller/ProposalController.java
@@ -1,31 +1,40 @@
 package org.br.mineradora.controller;
 
+import io.quarkus.security.Authenticated;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.persistence.GeneratedValue;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import org.br.mineradora.dto.ProposalDetailsDTO;
 import org.br.mineradora.service.ProposalService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Path("api/proposta")
+@Authenticated
 public class ProposalController {
 
     private final Logger LOG = LoggerFactory.getLogger(ProposalController.class);
+
+    @Inject
+    JsonWebToken jsonWebToken;
 
     @Inject
     ProposalService proposalService;
 
     @GET
     @Path("/{id}")
+    @RolesAllowed({"user","manager"})
     public ProposalDetailsDTO findDetailsProposal(@PathParam("id") long id) {
         return proposalService.findFullProposal(id);
     }
 
     @POST
-//    @RolesAllowed("")
+    @Transactional
+    @RolesAllowed("proposal-customer")
     public Response createProposal(ProposalDetailsDTO proposalDetailsDTO) {
 
         LOG.info("--- Recebendo proposta de compra ---");
@@ -45,7 +54,7 @@ public class ProposalController {
 
     @DELETE
     @Path("/{id}")
-//    @RolesAllowed("manager")
+    @RolesAllowed("manager")
     public Response removeProposal(@PathParam("id") long id) {
 
         try {
--- src/main/java/org/br/mineradora/entity/ProposalEntity.java
+++ src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -15,7 +15,7 @@ import java.util.Date;
 public class ProposalEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String customer;
